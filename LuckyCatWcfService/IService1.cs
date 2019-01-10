7c3e1cff7215ccb177c0586eb9076ef8929a4fe5
Phoenix Qing Ge <33667624+dylankeh@users.noreply.github.com>
2019-01-10 11:18:43 -0500

    Add files via upload

---
+++ LuckyCatWcfService/IService1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace LuckyCatWcfService
+{
+    /// <summary>
+    /// This web service receives a string type 'percentage' and return a double type 'discount'
+    /// 
+    /// Created by: Qing Ge
+    /// </summary>
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        double GetDiscount(string percentage);
+       
+    }
+    
+}
---
+++ LuckyCatWcfService/Service1.svc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace LuckyCatWcfService
+{
+    /// <summary>
+    /// This class uses switch function to get corresponding discount.
+    /// 
+    /// Created by: Qing Ge
+    /// </summary>
+    public class Service1 : IService1
+    {
+        public double GetDiscount(string percentage)
+        {
+            double discount;
+            switch (percentage)
+            {
+                case "Holiday Discount -20%":
+                    discount = 0.8;
+                    break;
+                case "Old Customer -25%":
+                    discount = 0.75;
+                    break;
+                case "Pay Cash -10%":
+                    discount = 0.9;
+                    break;
+                default:
+                    discount = 1;
+                    break;
+            }
+            return discount;
+        }
+    }
+}
